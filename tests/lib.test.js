6e596d2d0e0861422907d6b70f7bc3fefdcf57ba
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-27 14:19:23 +0300

    add changes

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -4,3 +4,8 @@ test("absolute - should return positive number if input is positive", () => {
   const result = lib.absolute(1);
   expect(result).toBe(1);
 });
+
+test("absolute - should return positive number if input is negative", () => {
+  const result = lib.absolute(-1);
+  expect(result).toBe(1);
+});
