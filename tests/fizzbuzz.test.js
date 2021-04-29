50e90f3a97bf53102de67e783eb97c00c64c0f47
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-29 17:07:06 +0300

    add new test case

--- tests/fizzbuzz.test.js
+++ tests/fizzbuzz.test.js
@@ -20,4 +20,19 @@ describe("FizzBuzz", () => {
     const result = lib.fizzBuzz(15);
     expect(result).toBe("FizzBuzz");
   });
+
+  it("should return Fizz if the input is divisible by only 3", () => {
+    const result = lib.fizzBuzz(3);
+    expect(result).toBe("Fizz");
+  });
+
+  it("should return Buzz if the input is divisible by only 5", () => {
+    const result = lib.fizzBuzz(5);
+    expect(result).toBe("Buzz");
+  });
+
+  it("should return input if the input is not divisible by 3 or 5", () => {
+    const result = lib.fizzBuzz(15);
+    expect(result).toBe("FizzBuzz");
+  });
 });
